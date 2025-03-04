f729bd4832b00d836263295b51401f0ff5c9aa83
antonio.lupu <antonio.lupu@herensys.com>
2025-03-04 16:29:07 +0200

    fix(offer-section): remove second step if exportRoute

--- src/app/lib/hooks/useOfferSteps.ts
+++ src/app/lib/hooks/useOfferSteps.ts
@@ -25,8 +25,12 @@ export function useOfferSteps(route: string, content) {
         .replace('{max}', max)
         .replace('{location}', location[locale]),
     }))
-    .slice(0, route === 'exportFrantaBelgia' ? -1 : undefined) // Remove last step if needed
-    .filter((step: Step) => exportRoutes.has(route) || step.stepNumber !== 4) // Remove step 4 if not an export route
+    .slice(0, route === 'exportFrantaBelgia' ? -1 : undefined) // Remove last step if exportFrantaBelgia
+    .filter(
+      (step: Step) =>
+        !(exportRoutes.has(route) && step.stepNumber === 2) &&
+        (!exportRoutes.has(route) || step.stepNumber !== 4),
+    ) // Remove step 4 if not an export route, if export route remove step 2
     .map((step: Step, index: number) => ({
       ...step,
       stepNumber: index + 1, // Reset step numbers
