f4f5d944bbfd1e28e8ba73de7437230245df7c2c
Antonio <antoniolupu72@gmail.com>
2025-03-09 02:46:43 +0200

    fix(performance): add dynamic imports for AboutImage(keep unmodified for images from catalog/export page)

--- src/app/components/globals/about/AboutImage.tsx
+++ src/app/components/globals/about/AboutImage.tsx
@@ -2,8 +2,13 @@
 
 import type { CSSProperties } from "react";
 import React, { useState } from "react";
-import Image from "next/image";
 import styles from "@/app/styles/globals/aboutSection.module.scss";
+import dynamic from "next/dynamic";
+
+const OptimizedImage = dynamic(() => import("next/image"), {
+  ssr: false,
+  loading: () => <div className="imageLoading" />,
+});
 
 function AboutImage({
   src,
@@ -16,7 +21,7 @@ function AboutImage({
   return (
     <div className={`${styles.card} ${styles.imageBox}`}>
       {isLoading && <div className="imageBlur" />}
-      <Image
+      <OptimizedImage
         sizes="100vw"
         alt="About image"
         width={100}
