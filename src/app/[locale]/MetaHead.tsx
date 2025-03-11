37ac35d89afb25ede4dd0a440cfeab381be64251
antonio.lupu <antonio.lupu@herensys.com>
2025-03-11 17:07:49 +0200

    fix(meta-head): edit component2

--- src/app/[locale]/MetaHead.tsx
+++ src/app/[locale]/MetaHead.tsx
@@ -1,40 +1,40 @@
-'use client';
-
 import React from 'react';
 import Head from 'next/head';
 
 function MetaHead({ structuredData }: { structuredData: object }) {
   return (
-    <Head>
-      <meta
-        name="google-site-verification"
-        content="0OsCdd_J0HLeN007NIOlwLs7nst7c-uQSlfAXqi1VA8"
-      />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify(structuredData),
-        }}
-      />
-      <link
-        rel="preload"
-        as="image"
-        href="/assets/images/hero-section.webp"
-        type="image/webp"
-        fetchPriority="high"
-      />
-      {/* ✅ Preload Google Fonts for Better LCP */}
-      <link
-        rel="preconnect"
-        href="https://fonts.googleapis.com"
-        crossOrigin="anonymous"
-      />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="anonymous"
-      />
-    </Head>
+    <head>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="0OsCdd_J0HLeN007NIOlwLs7nst7c-uQSlfAXqi1VA8"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(structuredData),
+          }}
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="/assets/images/hero-section.webp"
+          type="image/webp"
+          fetchPriority="high"
+        />
+        {/* ✅ Preload Google Fonts for Better LCP */}
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+      </Head>
+    </head>
   );
 }
 
