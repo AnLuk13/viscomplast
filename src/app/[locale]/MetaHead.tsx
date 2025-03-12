b99404b756c4d102318d92a0513178391fd3a47b
antonio.lupu <antonio.lupu@herensys.com>
2025-03-12 12:07:54 +0200

    fix(meta-head): remove redundant next/head

--- src/app/[locale]/MetaHead.tsx
+++ src/app/[locale]/MetaHead.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import Head from 'next/head';
 
 function MetaHead() {
   return (
     <head>
-      <Head>
-        <link
-          rel="preload"
-          as="image"
-          href="/assets/images/hero-section.webp"
-          type="image/webp"
-          fetchPriority="high"
-        />
-        {/* ✅ Preload Google Fonts for Better LCP */}
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-      </Head>
+      <link
+        rel="preload"
+        as="image"
+        href="/assets/images/hero-section.webp"
+        type="image/webp"
+        fetchPriority="high"
+      />
+      {/* ✅ Preload Google Fonts for Better LCP */}
+      <link
+        rel="preconnect"
+        href="https://fonts.googleapis.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
     </head>
   );
 }
